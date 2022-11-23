2eaf7d05117b59c70e082ff5b4f9a6dbfa34e904
Sunit47 <Sunitajgar47@gmail.com>
2022-11-23 09:00:30 +0530

    Refactored all the 3 to One Generic Method with Parameterized Constructor

--- com/blz/MaximumNumber.java
+++ com/blz/MaximumNumber.java
@@ -1,6 +1,15 @@
 package com.blz;
 
-public class MaximumNumber {
+public class MaximumNumber<E extends Comparable<E>> {
+
+	E a, b, c;
+
+	public MaximumNumber(E a, E b, E c) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+	}
+
 	public static void main(String[] args) {
 		System.out.println("The maximum number between 3 integer is : " + findMaximum(12, 20, 9));
 		System.out.println("The maximum number between 3 float numbers  is : " + findMaximum(15.5f, 12.2f, 9.5f));
