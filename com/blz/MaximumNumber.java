c8a5571f75822317e8d59a59157e5723452dd092
Sunit47 <Sunitajgar47@gmail.com>
2022-11-23 08:32:53 +0530

    finding maximum float number

--- com/blz/MaximumNumber.java
+++ com/blz/MaximumNumber.java
@@ -3,6 +3,7 @@ package com.blz;
 public class MaximumNumber {
 	public static void main(String[] args) {
 		System.out.println("The maximum number between 3 integer is : " + findMaximumNumber(12, 20, 9));
+		System.out.println("The maximum number between 3 float numbers  is : " + findMaximumNumber(15.5f, 12.2f, 9.5f));
 	}
 
 	public static int findMaximumNumber(Integer i, Integer j, Integer k) {
@@ -12,6 +13,17 @@ public class MaximumNumber {
 		}
 		if (k.compareTo(max) > 0) {
 			max = k;
+		}
+		return max;
+	}
+
+	public static float findMaximumNumber(Float x, Float y, Float z) {
+		Float max = x;
+		if (y.compareTo(max) > 0) {
+			max = y;
+		}
+		if (z.compareTo(max) > 0) {
+			max = z;
 		}
 		return max;
 	}
