45a40fc72b6de52dfabd4c97a3f415ffbca8ab29
Sunit47 <Sunitajgar47@gmail.com>
2022-11-23 08:41:54 +0530

    finding maximum between 3 string

--- com/blz/MaximumNumber.java
+++ com/blz/MaximumNumber.java
@@ -4,6 +4,7 @@ public class MaximumNumber {
 	public static void main(String[] args) {
 		System.out.println("The maximum number between 3 integer is : " + findMaximumNumber(12, 20, 9));
 		System.out.println("The maximum number between 3 float numbers  is : " + findMaximumNumber(15.5f, 12.2f, 9.5f));
+		System.out.println("The maximum between 3 String is : " + findMaximum("Java", "Springboot", "Gitbash"));
 	}
 
 	public static int findMaximumNumber(Integer i, Integer j, Integer k) {
@@ -24,6 +25,17 @@ public class MaximumNumber {
 		}
 		if (z.compareTo(max) > 0) {
 			max = z;
+		}
+		return max;
+	}
+
+	public static String findMaximum(String a, String b, String c) {
+		String max = a;
+		if (b.compareTo(max) > 0) {
+			max = b;
+		}
+		if (c.compareTo(max) > 0) {
+			max = c;
 		}
 		return max;
 	}
